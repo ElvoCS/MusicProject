a3d654260f3edf77687cf80ae44c193c657eb441
Terry D'Arcy <terrykdarcy@gmail.com>
2020-11-08 04:39:32 +0000

    added data cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import CardArea from "./components/CardArea";
-import { Button } from "@material-ui/core";
+import { Button, Card } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
+      <div class="background-image"></div>
       <Header />
       <CardArea />
     </div>
--- src/components/CardArea.js
+++ src/components/CardArea.js
@@ -1,13 +1,53 @@
 import React from "react";
 import "./CardArea.css";
 import MusicNoteIcon from "@material-ui/icons/MusicNote";
-import { AppBar, Toolbar, IconButton } from "@material-ui/core";
+import { Card, AppBar, Toolbar, makeStyles } from "@material-ui/core";
 import bg from "../res/bg.png"; // Tell webpack this JS file uses this image
+
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 function CardArea() {
   return (
-    <div className="cardArea">
-      <div className="card"> </div>
+    <div className="CardArea">
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <h1 className={"title"}>This is a data card</h1>
+      </Card>
+
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <h1 className={"title"}>This is a data card</h1>
+      </Card>
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <CardContent Style="background-color: #336bf2">
+          <h1 className={"title"}>This is a data card</h1>
+        </CardContent>
+      </Card>
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <CardContent Style="background-color: #336bf2">
+          <h1 className={"title"}>This is a data card</h1>
+        </CardContent>
+      </Card>
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <h1 className={"title"}>This is a data card</h1>
+      </Card>
+
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <CardContent Style="background-color: #336bf2">
+          <h1 className={"title"}>This is a data card</h1>
+        </CardContent>
+      </Card>
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <CardContent Style="background-color: #336bf2">
+          <h1 className={"title"}>This is a data card</h1>
+        </CardContent>
+      </Card>
+      <Card className="dataCard" Style="border-radius:30px;background-color: #336bf2">
+        <CardContent Style="background-color: #336bf2">
+          <h1 className={"title"}>This is a data card</h1>
+        </CardContent>
+      </Card>
     </div>
   );
 }
