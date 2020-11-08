16fb5b508900aa12576e50b3cdda987ce85ac96a
ElvoCS <55958363+ElvoCS@users.noreply.github.com>
2020-11-08 16:39:50 +0000

    media Player building

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from "./components/Header";
 import CardArea from "./components/CardArea";
 import Footer from "./components/Footer";
 import { Button, Card } from "@material-ui/core";
+import MediaControlCard from "./components/MediaControlCard";
 
 function App() {
   return (
     <div className="App">
       <div class="background-image"></div>
       <Header />
+      <MediaControlCard />
       <CardArea />
       <Footer />
     </div>
---
+++ src/components/MediaControlCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import "./MediaControlCard.css";
+
+function MediaControlCard() {
+  return (
+    <div className="details">
+      <Card
+        className="Card"
+        Style="border-radius:30px;background-color: #336bf2; margin-top:80px;"
+      >
+        <CardContent className="mediaContent__container">
+          <div className="mediaContent">
+            <Typography component="h5" variant="h5">
+              Buffalo Soldier
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Bob Marley
+            </Typography>
+          </div>
+        </CardContent>
+
+        <div className="dataCard__mediaControlsContainer">
+          <div className="dataCard_mediaControls">
+            <IconButton className="previous">
+              <SkipPreviousIcon />
+            </IconButton>
+
+            <IconButton className="play">
+              <PlayArrowIcon />
+            </IconButton>
+
+            <IconButton className="next">
+              <SkipNextIcon />
+            </IconButton>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default MediaControlCard;
