7e4d563719daa2321577d4a9f9ada66131a8f51e
Terry D'Arcy <terrykdarcy@gmail.com>
2020-11-19 21:52:30 +0000

    update

--- src/App.js
+++ src/App.js
@@ -8,90 +8,35 @@ import Footer from "./components/Footer";
 
 const App = () => {
   const spotify = Credentials();
-
-  console.log("RENDERING APP.JS");
-
   const [token, setToken] = useState("");
-  const [genres, setGenres] = useState({
-    selectedGenre: "",
-    listOfGenresFromAPI: [],
-  });
-  const [playlist, setPlaylist] = useState({
-    selectedPlaylist: "",
-    listOfPlaylistFromAPI: [],
-  });
   const [tracks, setTracks] = useState({
     selectedTrack: "",
     listOfTracksFromAPI: [],
   });
+  var elements = [];
+  const [musicData, setmusicData] = useState([]);
   const [trackDetail, setTrackDetail] = useState(null);
 
   useEffect(() => {
     axios("https://accounts.spotify.com/api/token", {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
+        Authorization: "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
       },
       data: "grant_type=client_credentials",
       method: "POST",
     }).then((tokenResponse) => {
       setToken(tokenResponse.data.access_token);
-
-      axios("https://api.spotify.com/v1/browse/categories?locale=sv_US", {
-        method: "GET",
-        headers: { Authorization: "Bearer " + tokenResponse.data.access_token },
-      }).then((genreResponse) => {
-        setGenres({
-          selectedGenre: genres.selectedGenre,
-          listOfGenresFromAPI: genreResponse.data.categories.items,
-        });
-      });
     });
-  }, [genres.selectedGenre, spotify.ClientId, spotify.ClientSecret]);
+  }, [spotify.ClientId, spotify.ClientSecret]);
 
-  const genreChanged = (val) => {
-    setGenres({
-      selectedGenre: val,
-      listOfGenresFromAPI: genres.listOfGenresFromAPI,
-    });
-
-    axios(
-      `https://api.spotify.com/v1/browse/categories/${val}/playlists?limit=10`,
-      {
-        method: "GET",
-        headers: { Authorization: "Bearer " + token },
-      }
-    ).then((playlistResponse) => {
-      setPlaylist({
-        selectedPlaylist: playlist.selectedPlaylist,
-        listOfPlaylistFromAPI: playlistResponse.data.playlists.items,
-      });
-    });
-
-    console.log(val);
-  };
-
-  const playlistChanged = (val) => {
-    console.log(val);
-    setPlaylist({
-      selectedPlaylist: val,
-      listOfPlaylistFromAPI: playlist.listOfPlaylistFromAPI,
-    });
-  };
-
-  const buttonClicked = (e) => {
-    e.preventDefault();
-
-    axios(
-      `https://api.spotify.com/v1/playlists/${playlist.selectedPlaylist}/tracks?limit=10`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
-    ).then((tracksResponse) => {
+  const getMostPopular = (e) => {
+    axios(`https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks`, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }).then((tracksResponse) => {
       setTracks({
         selectedTrack: tracks.selectedTrack,
         listOfTracksFromAPI: tracksResponse.data.items,
@@ -99,43 +44,32 @@ const App = () => {
     });
   };
 
-  const listboxClicked = (val) => {
-    const currentTracks = [...tracks.listOfTracksFromAPI];
-
-    const trackInfo = currentTracks.filter((t) => t.track.id === val);
-
-    setTrackDetail(trackInfo[0].track);
+  const loadElements = (e) => {
+    getMostPopular();
+    for (var i = 0; i < tracks.listOfTracksFromAPI.length; i++) {
+      //elements.push(tracks.listOfTracksFromAPI[i].track);
+      setmusicData(tracks.listOfTracksFromAPI[i].track);
+    }
+    console.log(elements);
   };
 
   return (
-    <div className="container">
-      <form onSubmit={buttonClicked}>
-        <Dropdown
-          label="Genre :"
-          options={genres.listOfGenresFromAPI}
-          selectedValue={genres.selectedGenre}
-          changed={genreChanged}
-        />
-        <Dropdown
-          label="Playlist :"
-          options={playlist.listOfPlaylistFromAPI}
-          selectedValue={playlist.selectedPlaylist}
-          changed={playlistChanged}
-        />
-        <div className="col-sm-6 row form-group px-0">
-          <button type="submit" className="btn btn-success col-sm-12">
-            Search
-          </button>
+    <div className="container" onLoad={getMostPopular}>
+      <div className="col-sm-6 row form-group px-0">
+        <button onClick={loadElements} className="btn btn-success col-sm-12">
+          load elements
+        </button>
+
+        <div Style="background-color: cream; width:100%; height:30%">
+          {elements.map((data, id) => {
+            return (
+              <div key={id}>
+                <h2>{data.name}</h2>
+              </div>
+            );
+          })}
         </div>
-        <div className="row">
-          <Listbox
-            items={tracks.listOfTracksFromAPI}
-            clicked={listboxClicked}
-          />
-          {trackDetail && <Detail {...trackDetail} />}
-        </div>
-      </form>
-      <Footer />
+      </div>
     </div>
   );
 };
